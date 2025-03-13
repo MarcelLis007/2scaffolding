7c8f88e4d0a95382286e6cbe190b9f6732ad3ad2
MarcelLis007 <marcelacm26@uniandes.edu.ec>
2025-03-12 20:27:14 -0500

    Primer commit del proyecto

---
+++ OneDrive - UNIANDES/SEMESTRES/NIVEL 6/FASE_2/PROGRAMACION WEB/Deberes programas/StoreGui/Models/Categorium.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreGui.Models;
+
+public partial class Categorium
+{
+    [Key]
+    [Column("CategoriaID")]
+    public int CategoriaId { get; set; }
+
+    [StringLength(50)]
+    public string Nombre { get; set; } = null!;
+
+    [StringLength(200)]
+    public string? Descripcion { get; set; }
+
+    public int? EdadMinima { get; set; }
+
+    [Column("RequiereID")]
+    public bool RequiereId { get; set; }
+
+    [InverseProperty("Categoria")]
+    public virtual ICollection<Producto> Productos { get; set; } = new List<Producto>();
+}
