3a2c05384ec0244032468cbf657faaef4d6e70f1
MarcelLis007 <marcelacm26@uniandes.edu.ec>
2025-03-12 20:35:53 -0500

    centaury

---
+++ OneDrive - UNIANDES/SEMESTRES/NIVEL 6/FASE_2/PROGRAMACION WEB/Deberes programas/SCAFFO/StoreGui/Models/DetalleVentum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreGui.Models;
+
+public partial class DetalleVentum
+{
+    [Key]
+    [Column("DetalleVentaID")]
+    public int DetalleVentaId { get; set; }
+
+    [Column("VentaID")]
+    public int VentaId { get; set; }
+
+    [Column("ProductoID")]
+    public int ProductoId { get; set; }
+
+    public int Cantidad { get; set; }
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal PrecioUnitario { get; set; }
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Subtotal { get; set; }
+
+    [ForeignKey("ProductoId")]
+    [InverseProperty("DetalleVenta")]
+    public virtual Producto Producto { get; set; } = null!;
+
+    [ForeignKey("VentaId")]
+    [InverseProperty("DetalleVenta")]
+    public virtual Ventum Venta { get; set; } = null!;
+}
